0b525296f3f62218087cf6aa18679b3b8417f883
Sumit Asthana <asthana.sumit23@gmail.com>
2015-03-08 18:02:41 +0530

    verification tweaked

--- register.php
+++ register.php
@@ -8,36 +8,36 @@ function errorRedirect( $msg, $url ) {
 	header('Location:'.$url.'?err='.$error);
 }
 if( $_SERVER['REQUEST_METHOD'] == "POST") {
-  $error = "";
-  if($_POST['captcha'] != $_SESSION['digit']) {
-  	$error = "Sorry! Inconsistent captcha";
-  	$_SESSION['err'] = $error;
-  	//errorRedirect($error,"register.php");
-  }else if( !check_csrf($_POST['CSRF']) ) {
-  	$error = "Sorry! Invalid request";
-  	$_SESSION['err'] = $error;
-  	//errorRedirect($error,"register.php");
-  }else if( !isset( $_POST['name'],$_POST['college'],$_POST['password'],$_POST['email'],$_POST['number'],$_POST['age']) ) {
-    $error = "One of the fields is missing";
-    $_SESSION['err'] = $error;
-    //header("Location:register.php");
-  } else if( empty($_POST['name']) || empty($_POST['college']) || empty($_POST['password']) || empty($_POST['email']) || empty($_POST['age']) ) {
-  	$error = "One of the fields is missing";
-  	$_SESSION['err'] = $error;
-    //errorRedirect($error,"register.php");
-  } else if( strlen( $_POST['name'] ) > 50 || strlen( $_POST['password'] ) > 50 ) {
-    $error = "name or password is too long(max 50)";
-    $_SESSION['err'] = $error;
-    //errorRedirect($error,"register.php");
-    //preg_match( '/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/', $_POST[ 'email' ] )
-  }else if ( !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)) {
-    $error = "Invalid email";
-    $_SESSION['err'] = $error;
-    //errorRedirect($error,"register.php");
-  } else if(!filter_var($_POST['age'],FILTER_VALIDATE_INT)) {
-  	$error = "Age must be numeric";
-  	$_SESSION['err'] = $error;
-  } /*else if(!filter_var($_POST['name'],FILTER_VALIDATE_REGEXP,'/[a-zA-Z_\.]/')) {
+	$error = "";
+	if($_POST['captcha'] != $_SESSION['digit']) {
+		$error = "Sorry! Inconsistent captcha";
+		$_SESSION['err'] = $error;
+		//errorRedirect($error,"register.php");
+	}else if( !check_csrf($_POST['CSRF']) ) {
+		$error = "Sorry! Invalid request";
+		$_SESSION['err'] = $error;
+		//errorRedirect($error,"register.php");
+	}else if( !isset( $_POST['name'],$_POST['college'],$_POST['password'],$_POST['email'],$_POST['number'],$_POST['age']) ) {
+		$error = "One of the fields is missing";
+		$_SESSION['err'] = $error;
+		//header("Location:register.php");
+	} else if( empty($_POST['name']) || empty($_POST['college']) || empty($_POST['password']) || empty($_POST['email']) || empty($_POST['age']) ) {
+		$error = "One of the fields is missing";
+		$_SESSION['err'] = $error;
+	//errorRedirect($error,"register.php");
+	} else if( strlen( $_POST['name'] ) > 50 || strlen( $_POST['password'] ) > 50 ) {
+		$error = "name or password is too long(max 50)";
+		$_SESSION['err'] = $error;
+		//errorRedirect($error,"register.php");
+		//preg_match( '/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/', $_POST[ 'email' ] )
+	}else if ( !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)) {
+		$error = "Invalid email";
+		$_SESSION['err'] = $error;
+		//errorRedirect($error,"register.php");
+	} else if(!filter_var($_POST['age'],FILTER_VALIDATE_INT)) {
+		$error = "Age must be numeric";
+		$_SESSION['err'] = $error;
+	} /*else if(!filter_var($_POST['name'],FILTER_VALIDATE_REGEXP,'/[a-zA-Z_\.]/')) {
   	$error = "Only letters,digits underscore and dot allowed in usernames";
   	$_SESSION['err'] = $error;
   }*/
@@ -53,57 +53,61 @@ if( $_SERVER['REQUEST_METHOD'] == "POST") {
   //$number = mysql_real_escape_string($_POST['number']);
   //$age = mysql_real_escape_string($_POST['age']);
   //http://www.sanwebe.com/2013/03/basic-php-mysqli-usage
-  $name = $_POST['name'];
-  $pass = $_POST['password'];
-  $query = "SELECT * FROM `users` "
-            . "WHERE name=? OR email = ?";
-  try{
-  	$stmt = $db->prepare($query);
-  	$stmt->execute(array($_POST['name'],$_POST['email']));
-  } catch( PDOException $e ) {
-  	echo "Query error ".$e->getMessage();
-  }
+  // proceed only if no error found
+	if( !isset( $_SESSION['err'] ) ) {
+		$name = $_POST['name'];
+		$pass = $_POST['password'];
+		$query = "SELECT * FROM `users` "
+		        . "WHERE name=? OR email = ?";
+		try{
+			$stmt = $db->prepare($query);
+			$stmt->execute(array($_POST['name'],$_POST['email']));
+		} catch( PDOException $e ) {
+			echo "Query error ".$e->getMessage();
+		}
 
-  $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-  if (count($result) != 0) {
-        $error = "Username or email already taken! Please choose another!";
-        $_SESSION['err'] = $error;
-        //errorRedirect($error,"register.php");
-        //header("Location:register.php");
-    }
-  if(!isset($_SESSION['err'])){
-    $len = 32;
-    //$salt = mcrypt_create_iv($len);
-    $hash = password_hash($pass,PASSWORD_DEFAULT);
-    $query = "INSERT INTO `users` (`name`,`email`,`college`,`number`,`age`,`pass`) ".
-    "VALUES (?,?,?,?,?,?)";
-    //http://stackoverflow.com/questions/2552545/mysqli-prepared-statements-error-reporting
-    try {
-    	$stmt = $db->prepare($query);
-    	$stmt->execute(array($_POST['name'],
-    		$_POST['email'],
-    		$_POST['college'],
-    		$_POST['number'],
-  			$_POST['age'],$hash
-    	));
-    } catch( PDOException $e ) {
-    	echo "Query error ".$e->getMessage();
-    }
-    if($db->errorCode()!='0') {
-    	$_SESSION['err'] = "Registration error ".$db->errorCode();
-    } else {
-    	//echo var_dump($db->errorInfo());
-	    $_SESSION['user'] = $name.'    ';
-	    //echo $stmt->error;
-	    //echo "Done";
-	    $mailTo = $_POST['email'];
-	    $txt = "You have been successfully registered for eweek.\nYour username is ".$name;
-	    $txt .= "\nYour password is ".$pass;
-	    $sub = "IIT Patna E-Week registrations";
-	    mail($mailTo,$sub,$txt);
-	    header('Location:profile.php');
-    }
-  }
+		$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		if (count($result) != 0) {
+			$error = "Username or email already taken! Please choose another!";
+			$_SESSION['err'] = $error;
+			//errorRedirect($error,"register.php");
+			//header("Location:register.php");
+		}
+		if(!isset($_SESSION['err'])){
+			$len = 32;
+			//$salt = mcrypt_create_iv($len);
+			$hash = password_hash($pass,PASSWORD_DEFAULT);
+			$query = "INSERT INTO `users` (`name`,`email`,`college`,`number`,`age`,`pass`) ".
+			"VALUES (?,?,?,?,?,?)";
+			//http://stackoverflow.com/questions/2552545/mysqli-prepared-statements-error-reporting
+			try {
+				$stmt = $db->prepare($query);
+				$stmt->execute(array($_POST['name'],
+					$_POST['email'],
+					$_POST['college'],
+					$_POST['number'],
+						$_POST['age'],$hash
+				));
+			} catch( PDOException $e ) {
+				echo "Query error ".$e->getMessage();
+			}
+			if($db->errorCode()!='0') {
+				$_SESSION['err'] = "Registration error ".$db->errorCode();
+			} else {
+				//echo var_dump($db->errorInfo());
+				$_SESSION['user'] = $name.'    ';
+				//echo $stmt->error;
+				//echo "Done";
+				$mailTo = $_POST['email'];
+				$txt = "You have been successfully registered for eweek.\nYour username is ".$name;
+				$txt .= "\nYour password is ".$pass;
+				$sub = "IIT Patna E-Week registrations";
+				mail($mailTo,$sub,$txt);
+				header('Location:profile.php');
+			}
+		}
+	}
+
 } else if( $_SERVER['REQUEST_METHOD'] === "GET" ) {
   if( isset( $_GET['err'] ) ) {
     $_SESSION['err'] = $_GET['err'];
